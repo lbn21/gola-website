9c27895530d419b1919c1fa3d94895c9a3520365
Jakub Marciniszyn <jakub.marciniszyn@tfs.com>
2022-07-01 14:31:42 +0100

    Adds basic bootstrap nav-bar and offcanvas element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,39 @@
-import { Component } from '@angular/core';
-import { NgbConfig } from '@ng-bootstrap/ng-bootstrap';
+import { Component, Input } from '@angular/core';
+import {
+  NgbActiveOffcanvas,
+  NgbConfig,
+  NgbOffcanvas,
+} from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-ngbd-offcanvas-content',
+  template: `
+    <div class="offcanvas-header">
+      <h5 class="offcanvas-title">Offcanvas</h5>
+      <button
+        type="button"
+        class="btn-close text-reset"
+        aria-label="Close"
+        (click)="activeOffcanvas.dismiss('Cross click')"
+      ></button>
+    </div>
+    <div class="offcanvas-body">
+      <div>Hello {{ name }}</div>
+      <button
+        type="button"
+        class="btn btn-outline-dark"
+        (click)="activeOffcanvas.close('Close click')"
+      >
+        Close
+      </button>
+    </div>
+  `,
+})
+export class NgbdOffcanvasContentComponent {
+  @Input() name = null;
+
+  constructor(public activeOffcanvas: NgbActiveOffcanvas) {}
+}
 
 @Component({
   selector: 'app-root',
@@ -9,7 +43,15 @@ import { NgbConfig } from '@ng-bootstrap/ng-bootstrap';
 export class AppComponent {
   title = 'gola-website';
 
-  constructor(ngbConfig: NgbConfig) {
+  constructor(ngbConfig: NgbConfig, private offcanvasService: NgbOffcanvas) {
     ngbConfig.animation = false;
+  }
+
+  open() {
+    const offcanvasRef = this.offcanvasService.open(
+      NgbdOffcanvasContentComponent,
+      { position: 'top' }
+    );
+    offcanvasRef.componentInstance.name = 'Gosia';
   }
 }
