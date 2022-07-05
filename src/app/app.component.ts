752193b33d16894e148d362790a5c9607b9b2360
Jakub Marciniszyn <jakub.marciniszyn@tfs.com>
2022-07-05 11:48:10 +0100

    Removes offcanvas title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import {
   selector: 'app-ngbd-offcanvas-content',
   template: `
     <div class="offcanvas-header">
-      <h5 class="offcanvas-title">Offcanvas</h5>
+      <h5 class="offcanvas-title">&nbsp;</h5>
       <button
         type="button"
         class="btn-close text-reset"
