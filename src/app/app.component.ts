f0edc1b10b8e7ce631d2e7281ddff11e69370ba3
Jakub Marciniszyn <71651055+lbn21@users.noreply.github.com>
2023-01-04 16:53:00 +0000

    removes gtag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,8 @@
-import { isPlatformBrowser } from '@angular/common';
-import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
-import { filter } from 'rxjs';
-
-declare let gtag: Function;
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  constructor(
-    @Inject(PLATFORM_ID) private platformId: Object,
-    private router: Router
-  ) {}
-
-  ngOnInit(): void {
-    if (isPlatformBrowser(this.platformId)) {
-      this.router.events
-        .pipe(
-          filter(
-            (event): event is NavigationEnd => event instanceof NavigationEnd
-          )
-        )
-        .subscribe((event: NavigationEnd) => {
-          gtag('config', 'G-H9VLYY0PK0', {
-            page_path: event.urlAfterRedirects,
-          });
-        });
-    }
-  }
-}
+export class AppComponent {}
